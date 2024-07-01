1563d4e88c05c5080cf6d718832431540d522ad1
B21020 <e1b21020@oit.ac.jp>
2024-07-01 11:58:27 +0900

    tests

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -1,7 +1,39 @@
-package main.java.org.example;
+package org.example;
 
 public class Calculate {
-    public int sum(int x, int y){
-        return x + y;
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public double average(int a, int b) {
+        return (a + b) / 2.0;
+    }
+
+    public int sumFromOneToTen() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public double averageFromOneToTen() {
+        return sumFromOneToTen() / 10.0;
+    }
+
+    public int sumOddFromOneToTen() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i += 2) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int sumEvenFromOneToTen() {
+        int sum = 0;
+        for (int i = 2; i <= 10; i += 2) {
+            sum += i;
+        }
+        return sum;
     }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -1,12 +1,48 @@
-package test.java.org.example;
+package org.example;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculateTest {
     @Test
     public void testSum() {
-        Calculate Calculate = new Calculate();
-            int expected = 5;
-            assertEquals(expected, Calculate.sum(2, 3));
-     }
+        Calculate calculate = new Calculate();
+        int expected = 5;
+        assertEquals(expected, calculate.sum(2, 3));
+    }
+
+    @Test
+    public void testAverage() {
+        Calculate calculate = new Calculate();
+        double expected = 2.5;
+        assertEquals(expected, calculate.average(2, 3), 0.001);
+    }
+
+    @Test
+    public void testSumFromOneToTen() {
+        Calculate calculate = new Calculate();
+        int expected = 55;
+        assertEquals(expected, calculate.sumFromOneToTen());
+    }
+
+    @Test
+    public void testAverageFromOneToTen() {
+        Calculate calculate = new Calculate();
+        double expected = 5.5;
+        assertEquals(expected, calculate.averageFromOneToTen(), 0.001);
+    }
+
+    @Test
+    public void testSumOddFromOneToTen() {
+        Calculate calculate = new Calculate();
+        int expected = 25;
+        assertEquals(expected, calculate.sumOddFromOneToTen());
+    }
+
+    @Test
+    public void testSumEvenFromOneToTen() {
+        Calculate calculate = new Calculate();
+        int expected = 30;
+        assertEquals(expected, calculate.sumEvenFromOneToTen());
+    }
 }
